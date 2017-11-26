35c168fd116201ae55c38d20919d31d630bbc6bd
root <root@shashiudawa5.mylabserver.com>
2017-11-26 08:21:48 +0000

    correct some typo

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,17 +1,17 @@
 class tomcat::config(
  String $config_name     = $::tomcat::config_name,
  String $config_path     = $::tomcat::config_path,
- String $config_owner    = $tomcat::config_owner,
- String $config_group    = $::tomcat::group,
- String $config_mode     = $::tomcat::mode,
+ String $config_owner    = $::tomcat::config_owner,
+ String $config_group    = $::tomcat::config_group,
+ String $config_mode     = $::tomcat::config_mode,
 ) {
   file { 'tomcat_config_file':
     ensure => $config_ensure,
     name   => $config_name,
     path   => $config_path,
-    owner  => $owner,
-    group  => $group,
-    mode   => $mode,
+    owner  => $config_owner,
+    group  => $config_group,
+    mode   => $config_mode,
     source => 'puppet:///modules/tomcat/tomcat.conf',
   }
 }
