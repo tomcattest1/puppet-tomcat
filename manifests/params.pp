44c9b455487f111e427c4ab15ebe69e318841b02
root <root@shashiudawa5.mylabserver.com>
2017-11-26 08:24:49 +0000

    correct

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class tomcat::params {
  $package_ensure = 'installed'
  $config_ensure  = 'file'
  $config_name    = 'tomcat.conf'
- $config_path    = 'etc/tomcat/tomcat.conf'
+ $config_path    = '/etc/tomcat/tomcat.conf'
  $config_owner   = 'tomcat'
  $config_group   = 'tomcat'
  $config_mode    = '0755'
