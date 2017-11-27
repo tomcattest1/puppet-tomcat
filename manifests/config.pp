16f284a8fd4b8990cbef57b11ee20b4f4358b5bf
root <root@shashiudawa5.mylabserver.com>
2017-11-27 00:27:58 +0000

    fix config

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,9 +1,22 @@
 class tomcat::config(
- String $config_name     = $::tomcat::config_name,
- String $config_path     = $::tomcat::config_path,
- String $config_owner    = $::tomcat::config_owner,
- String $config_group    = $::tomcat::config_group,
- String $config_mode     = $::tomcat::config_mode,
+ String $config_name        = $::tomcat::config_name,
+ String $config_path        = $::tomcat::config_path,
+ String $config_owner       = $::tomcat::config_owner,
+ String $config_group       = $::tomcat::config_group,
+ String $config_mode        = $::tomcat::config_mode,
+ Integer $tomcat_cfg_loaded = $::tomcat::tomcat_cfg_loaded
+ String $tomcats_base       = $::tomcat::tomcats_base
+ String $java_home          = $::tomcat::java_home
+ String $xms                = $::tomcat::xms
+ String $xmx                = $::tomcat::xmx
+ String $catalina_base      = $::tomcat::catalina_base
+ String $catalina_home      = $::tomcat::catalina_home
+ String $jasper_home        = $::tomcat::jasper_home
+ String $catalina_tmpdir    = $::tomcat::catalina_tmpdir
+ String $tomcat_user        = $::tomcat::tomcat_user
+ Boolean $security_manager  = $::tomcat::security_manager
+ Integer $shutdown_wait     = $::tomcat::shutdown_wait
+ Boolean $shutdown_verbose  = $::tomcat::shutdown_verbose
 ) {
   file { 'tomcat_config_file':
     ensure  => $config_ensure,
@@ -12,6 +25,6 @@ class tomcat::config(
     owner   => $config_owner,
     group   => $config_group,
     mode    => $config_mode,
-    content => template('tomcat/tomcat.conf.erb'),
+    content => template("${module_name}/tomcat.conf.erb"),
   }
 }
